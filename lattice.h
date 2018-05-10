7af17439e86f77c076cf3bc035972acc95b3cf48
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-10 20:06:05 +0100

    BGK Feq, getRho done.

--- lattice.h
+++ lattice.h
@@ -1,4 +1,8 @@
+#ifndef LATTICE_HH
+#define LATTICE_HH
+// Choosing the lattice
 #define lattice D2Q9
+
 #include<vector>
 #include<iostream>
 
@@ -10,10 +14,15 @@ const int nQ = 9;
 // Lattice dimensions
 const int nD = 2;
 
-// Lattice vectors with the index of Q =    0      1       2        3      4
-const int Qvectors[nQ][nD]  =          {  {0,0}, {1,0},  {0,1},  {-1,0}, {0,-1},
-//                                          5       6       7       8
-                                          {1,1}, {-1,1}, {-1,-1}, {1,-1}};
+// Squared of micro-velocity (c)
+const double cSqr = 1.;
+const double cQad = 1.;
+
+// Lattice vectors, which are usually shown by "ei" in the literature.
+// In SIMPLB, for the sake of being easily identified, they are called Qvectors.
+// With the indexes of iQ =     0      1       2        3      4
+const int Qvector[nQ][nD]  = {{0,0}, {1,0},  {0,1},  {-1,0}, {0,-1}, //                              5       6       7       8
+                              {1,1}, {-1,1}, {-1,-1}, {1,-1}};
 
 // Index of opposite vectors of {0, 1, 2, 3, 4, 5, 6, 7, 8}
 const int iOpposite[nQ] =       {0, 3, 4, 1, 2, 7, 8, 5, 6};
@@ -26,3 +35,4 @@ const double weight[nQ] = {(double) 4./9.,
 		(double) 1./9., (double) 1./9.,(double) 1./9.,(double) 1./9.,
 		(double) 1./36., (double) 1./36., (double) 1./36., (double) 1./36.};
 }
+#endif
