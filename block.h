23b64eff4af8b61c7b97a8b1178ef64260304f19
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-19 17:09:03 +0100

    nX,nY,size => origin, dimension

--- block.h
+++ block.h
@@ -36,42 +36,42 @@ public:
 	       dim[iD]=dim_[iD];
 	       vol*=dim_[iD];
         }
-		
+
         // Memory allocation for nodes
         nodes = new Node[vol];
 
-	//  Set loop limits for actual and ghost  boundaries 
+	//  Set loop limits for actual and ghost  boundaries
         setBoundaryLimit();
         setGhostLimit();
-		
+
     }
 
     Node& operator() (const int& iX, const int& iY){
-        return nodes[getIndex(iX,iY)];
+        return nodes[getShortIndex(iX,iY)];
     }
 
 	Node const& operator() (const int& iX, const int& iY) const {
-        return nodes[getIndex(iX,iY)];
+        return nodes[getShortIndex(iX,iY)];
     }
 
-    int getIndex (const int& iX, const int& iY) const {return iX*nY+iY;}
+    int getShortIndex (const int& iX, const int& iY) const {return iX*dim[1]+iY;}
 
-		
+
 
     Node& operator[] (const int& iNode){return nodes[iNode];}
 
-    int getNX() const {return nX;}
-    int getNY() const {return nY;}
-    int getSize() const {return size;}
+    int& getDim(iD) const {return dim[iD];}
+    int& getOrigin(iD) const {return origin[iD];}
+    int& getVol() const {return vol;}
 
     // Gives the neighbor block (or MPI process) in the direction of iQ.
     int& getMPIneighbor (const int& iQ){return MPIneighbor[iQ];}
 
     // Prints class members
     void print() {
-        cout<< "nX = " << nX<<endl;
-        cout<< "nY = " << nY<<endl;
-        cout<< "size  = " << size<<endl;
+        cout<< "DimX = " << getDim(0)<<endl;
+        cout<< "DimY = " << getDim(1)<<endl;
+        cout<< "Volume  = " << vol<<endl;
         for (int iQ=0;iQ<lattice::nQ;++iQ){
             cout<<"iQ="<<iQ<<"boundaryLimit iXbegin="<<boundaryLimit[iQ].getBegin(0)<<" iXend="<<boundaryLimit[iQ].getEnd(0)
                 <<"  iYbegin="<<boundaryLimit[iQ].getBegin(1)<<" iYend="<<boundaryLimit[iQ].getEnd(1)<<endl;
@@ -87,34 +87,32 @@ public:
 
     // Finds the neighbor of the node (iX,iY) in the direction of iQ for a periodic block.
     void getPeriodicNeighbor (const int& iX, const int& iY, const int& iQ, int& iX_neighbor, int& iY_neighbor) {
-        iX_neighbor = (iX + lattice::Qvector[iQ][0]+nX)%nX;
-        iY_neighbor = (iY + lattice::Qvector[iQ][1]+nY)%nY;
+        iX_neighbor = (iX + lattice::Qvector[iQ][0]+dim[0])%dim[0];
+        iY_neighbor = (iY + lattice::Qvector[iQ][1]+dim[1])%dim[1];
     }
 
     // Stream in reversed direction over the whole block. The mirror() function, after this can
     // correct them.
     void revStream(){
-        int id, id_neighbor;
-        int iX_neighbor,iY_neighbor;
-        int iQ;
-		
-        for (int iX=0;iX<nX;++iX){
-            for (int iY=0;iY<nY;++iY){
-                id = getIndex(iX,iY);
+
+        for (int iX=0;iX<dim[0];++iX){
+            for (int iY=0;iY<dim[1];++iY){
+                int id = getShortIndex(iX,iY);
                 for (int i=0;i<lattice::nQ/2;++i){
 
-                    iQ = lattice::iHalfQs[i];
+                    int iQ = lattice::iHalfQs[i];
+                    int iX_neighbor,iY_neighbor;
                     getPeriodicNeighbor(iX,iY,iQ,iX_neighbor,iY_neighbor);
-                    id_neighbor = getIndex(iX_neighbor,iY_neighbor); 
+                    int id_neighbor = getShortIndex(iX_neighbor,iY_neighbor);
                     std::swap(nodes[id][iQ],nodes[id_neighbor][lattice::iOpposite[iQ]]);
                 }
             }
         }
     }
-	
+
     // Mirroring distros all over the block.
     void mirror(){
-        for (int iNode=0;iNode<size;++iNode){
+        for (int iNode=0;iNode<vol;++iNode){
             nodes[iNode].mirror();
         }
     }
@@ -126,7 +124,7 @@ public:
 
     Block& operator= (const double& rhs){
 
-        for (int iNode=0;iNode<size;++iNode){
+        for (int iNode=0;iNode<vol;++iNode){
             nodes[iNode]= rhs;
         }
     }
@@ -142,22 +140,22 @@ public:
         int iXend,iYend,iXbegin,iYbegin;
         for (int iQ=0;iQ<lattice::nQ;++iQ){
             if (lattice::Qvector[iQ][0]==1){
-                iXbegin = nX-2;
-                iXend = nX-1;
+                iXbegin = dim[0]-2;
+                iXend = dim[0]-1;
             }else if(lattice::Qvector[iQ][0]==0){
                 iXbegin=1;
-                iXend=nX-1;
+                iXend=dim[0]-1;
             }else if(lattice::Qvector[iQ][0]==-1){
                 iXbegin=1;
                 iXend=2;
             }
 
             if (lattice::Qvector[iQ][1]==1){
-                iYbegin = nY-2;
-                iYend = nY-1;
+                iYbegin = dim[1]-2;
+                iYend = dim[1]-1;
             }else if(lattice::Qvector[iQ][1]==0){
                 iYbegin=1;
-                iYend=nY-1;
+                iYend=dim[1]-1;
             }else if(lattice::Qvector[iQ][1]==-1){
                 iYbegin=1;
                 iYend=2;
@@ -176,22 +174,22 @@ public:
         int iXbegin,iYbegin,iXend,iYend;
         for (int iQ=0;iQ<lattice::nQ;++iQ){
             if (lattice::Qvector[iQ][0]==1){
-                iXbegin = nX-1;
-                iXend = nX;
+                iXbegin = dim[0]-1;
+                iXend = dim[0];
             }else if(lattice::Qvector[iQ][0]==0){
                 iXbegin=1;
-                iXend=nX-1;
+                iXend=dim[0]-1;
             }else if(lattice::Qvector[iQ][0]==-1){
                 iXbegin=0;
                 iXend=1;
             }
 
             if (lattice::Qvector[iQ][1]==1){
-                iYbegin = nY-1;
-                iYend = nY;
+                iYbegin = dim[1]-1;
+                iYend = dim[1];
             }else if(lattice::Qvector[iQ][1]==0){
                 iYbegin=1;
-                iYend=nY-1;
+                iYend=dim[1]-1;
             }else if(lattice::Qvector[iQ][1]==-1){
                 iYbegin=0;
                 iYend=1;
