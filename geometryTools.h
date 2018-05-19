48c8a1f6a7ca40318da454df3e2a0a573ec448d9
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-19 01:56:11 +0100

    Box, LoopLimits separated

--- geometryTools.h
+++ geometryTools.h
@@ -3,51 +3,90 @@
 using namespace std;
 
 
-class Box{
+class TwoPoints{
 
-    int begin[lattice::nD];
-    int end[lattice::nD];
+    protected:
+    int point0[lattice::nD];
+    int point1[lattice::nD];
 
-public:
-
-    Box(){
+    public:
+    TwoPoints(){
        for (int iD=0;iD<lattice::nD;++iD){
-           begin[iD]=0;
-           end[iD] = 0;
+           point0[iD]=0;
+           point1[iD] = 0;
        }
     }
 
-    Box(const int& iXbegin,const int& iYbegin,const int& iXend,const int& iYend){
-      set(iXbegin,iYbegin,iXend,iYend);
+    TwoPoints(const int& iX0,const int& iY0,const int& iX1,const int& iY1){
+      set(iX0,iY0,iX1,iY1);
     }
-    void set (const int& iXbegin,const int& iYbegin,const int& iXend,const int& iYend){
-       begin[0]=iXbegin;
-       begin[1]=iYbegin;
-       begin[2]=0;
-       end[0] = iXend;
-       end[1] = iYend;
-       end[2] = 1;
+    // Setting 2D points
+    void set (const int& iX0,const int& iY0,const int& iX1,const int& iY1){
+       point0[0]=iX0;
+       point0[1]=iY0;
+
+       point1[0] = iX1;
+       point1[1] = iY1;
     }
 
-    // Begining of the loop over the box, or box origin
-    int& getBegin(const int& iD) {return begin[iD];}
-    // End of the loop over the box
-    int& getEnd  (const int& iD){return end[iD];}
-    // Get length of a dimension
-    int getL(const int& iD){return end[iD]-begin[iD];}
-    // Volume in 2D is surface area of the box
-    int getVol(){
-        int vol=1;
-        for (int iD=0;iD<lattice::nD;++iD){
-            vol*=getL(iD);
+    TwoPoints(const int& iX0,const int& iY0,const int& iZ0,const int& iX1,const int& iY1,const int& iZ1 ){
+      set(iX0,iY0,iZ0,iX1,iY1,iZ1);
+    }
+    // Setting 3D points
+    void set (const int& iX0,const int& iY0,const int& iZ0,const int& iX1,const int& iY1,const int& iZ1){
+       point0[0]=iX0;
+       point0[1]=iY0;
+       point0[2]=iZ0;
+
+       point1[0] = iX1;
+       point1[1] = iY1;
+       point1[2] = iZ1;
+    }
+    
+};
+
+class Box :public TwoPoints{
+	public:
+	// Get origin of of the box
+	const int& getOrigin(const int& iD) const {return point0[iD];}
+	// Get dimensions of the box     	
+	const int& getDim(const int& iD) const {return point1[iD];}
+   	// Volume in 2D is surface area of the box
+   	int getVol(){
+        	int vol=1;
+        	for (int iD=0;iD<lattice::nD;++iD){
+            	vol*=getDim(iD);
         }
         return vol;
-    }
+	}
+	void print(){
+	for (int iD=0;iD<lattice::nD;++iD){
+	    cout<<" Axis="<<iD<<" Origin ="<<getOrigin(iD)<<" Dimension="<<getDim(iD)<<endl;
+	}
+	cout<<" Volume="<<getVol()<<endl;
+	}
+};
 
-    void print(){
-        for (int iD=0;iD<lattice::nD;++iD){
-            cout<<"Dimension="<<iD<<" Begin ="<<begin[iD]<<" End ="<<end[iD]<<" Length="<<getL(iD)<<endl;
-        }
-        cout<<" Volume="<<getVol()<<endl;
-    }
+class LoopLimit :public TwoPoints{
+	public:
+    	// Begining of the loop over the box, or box origin
+	const int& getBegin(const int& iD)const {return point0[iD];}
+	// End of the loop over the box
+	const int& getEnd  (const int& iD)const {return point1[iD];}
+	// Get length of a dimension
+	int getDim(const int& iD){return point1[iD]-point0[iD];}
+   	// Volume in 2D is surface area of the box
+   	int getVol(){
+        	int vol=1;
+        	for (int iD=0;iD<lattice::nD;++iD){
+            	vol*=getDim(iD);
+        	}
+        	return vol;
+	}
+	void print(){
+	for (int iD=0;iD<lattice::nD;++iD){
+	    cout<<"Axis="<<iD<<" Begin ="<<point0[iD]<<" End ="<<point1[iD]<<" Length="<<getDim(iD)<<endl;
+	}
+	cout<<" Volume="<<getVol()<<endl;
+	}
 };
