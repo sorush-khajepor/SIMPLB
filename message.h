ff8f72661680203e12ecd558dae22226b03e2124
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-24 14:52:02 +0100

    Message class added

---
+++ message.h
@@ -0,0 +1,20 @@
+#include "mpiTools.h"
+
+class Message(){
+    
+    
+    // Neighbor blocks (or MPI processes) in the directions of Qvectors in the MPI world.
+    // Note, each Block is assigned to an MPI process.
+    int neighbor[lattice::nQ];
+    
+public:
+    Message(const int neighbor[]_){
+        for (int iQ=0;iQ<lattice::nQ;iQ++){
+            neighbor[iQ]=neighbor_[iQ];
+        }
+    }
+    
+    // Gives the neighbor block (or MPI process) in the lattice direction of iQ.
+    int& getNeighbor (const int& iQ) {return Neighbor[iQ];}
+}
+
