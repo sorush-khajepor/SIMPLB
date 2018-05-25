001e4c21c38be72fee5227d83355e2e80d3d78df
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-25 01:22:03 +0100

    getNeighbor is const function now

--- message.h
+++ message.h
@@ -1,29 +1,26 @@
 #include "lattice.h"
 #include "mpiTools.h"
+#include "container.h"
 
 class Message{
     
     // Neighbor blocks (or MPI processes) in the directions of Qvectors in the MPI world.
     // Note, each Block is assigned to an MPI process.
-    int neighbor[lattice::nQ];
+    arrayNQ<int> neighbor;
     
 public:
-    Message(const int neighbor_[]){
-        for (int iQ=0;iQ<lattice::nQ;iQ++){
-            neighbor[iQ]=neighbor_[iQ];
-        }
+    Message(const arrayNQ<int> neighbor_){
+        neighbor=neighbor_;
     }
     
     // Gives the neighbor block (or MPI process) in the lattice direction of iQ.
-    int& getNeighbor (const int& iQ) {return neighbor[iQ];}
+    const int& getNeighbor (const int& iQ) const {return neighbor[iQ];}
+    const arrayNQ<int>& getNeighbor () const {return neighbor;}
 
     // Prints class members
     void print() {
-        for (int iQ=0;iQ<lattice::nQ;++iQ){
-            cout<<"iQ="<<iQ<<"MPI neighbor="<<neighbor[iQ];
-        }
+        neighbor.print();
     }
-
 
 
 
