075ba2659e8689e3ca9ed9ec85ce2bc04f199b7e
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-28 17:43:54 +0100

    SArray completed.

--- sArray.h
+++ sArray.h
@@ -1,115 +1,10 @@
-#ifndef CONTAINER_H
-#define CONTAINER_H
+#ifndef SARRAY_H
+#define SARRAY_H
 
 
 #include "lattice.h"
 
 #include<iostream>
-/*
-// A class containing only data array with the length of number of lattice velocities (nQ)
-template <class T>
-class arrayNQ{
-        T data[lattice::nQ];
-public:
-        arrayNQ(){
-            *this=0.;
-        }
-        arrayNQ(const T rhs[]){
-            *this=rhs;
-        }
-
-        arrayNQ& operator= (const T& rhs){
-                for (int iQ=0;iQ<lattice::nQ;iQ++){
-                    data[iQ] = rhs;
-                }
-            }
-        arrayNQ& operator= (const T rhs[]){
-                for (int iQ=0;iQ<lattice::nQ;iQ++){
-                    data[iQ] = rhs[iQ];
-                }
-            }
-        arrayNQ& operator= (const arrayNQ& rhs){
-                for (int iQ=0;iQ<lattice::nQ;iQ++){
-                    data[iQ] = rhs[iQ];
-                }
-            }
-        arrayNQ& operator= (const Node& rhs){
-                for (int iQ=0;iQ<lattice::nQ;iQ++){
-                    data[iQ] = rhs[iQ];
-                }
-            }
-        void copyTo(Node& lhs){
-                for (int iQ=0;iQ<lattice::nQ;iQ++){
-                     lhs[iQ]=data[iQ];
-                }
-        }
-        T& operator[](const int& iQ){
-            return data[iQ];
-        }
-        const T& operator[] (const int& iQ)const{
-            return data[iQ];
-        }
-
-        void print () {
-            for (int iQ=0;iQ<lattice::nQ;iQ++){
-                std::cout<< "data("<<iQ<<") = "<<data[iQ]<< std::endl;
-            }
-        }
-};
-
-
-// An array containing only cartesian data.
-template <class T>
-class arrayND{
-        T data[lattice::nD];
-public:
-        // Initialized with zero
-        arrayND(){
-            *this=0.;
-        }
-
-        // Initialized with a standard C array
-        arrayND(const T rhs[]){
-            *this=rhs;
-        }
-
-        // Filling array with one value
-        arrayND& operator= (const T& rhs){
-                for (int iD=0;iD<lattice::nD;iD++){
-                    data[iD] = rhs;
-                }
-            }
-        // Filling array with another arrayND
-        arrayND& operator= (const arrayND& rhs){
-                for (int iD=0;iD<lattice::nD;iD++){
-                    data[iD] = rhs[iD];
-                }
-            }
-        // Filling array with another standard c array
-        arrayND& operator= (const T rhs[]){
-                for (int iD=0;iD<lattice::nD;iD++){
-                    data[iD] = rhs[iD];
-                }
-            }
-        // An array member is called by its index
-        T& operator[](const int& iD){
-            return data[iD];
-        }
-
-        // Const call to an array member
-        const T& operator[] (const int& iD)const{
-            return data[iD];
-        }
-
-        // Print array on screen
-        void print () {
-            for (int iD=0;iD<lattice::nD;iD++){
-                std::cout<< "data("<<iD<<") = "<<data[iD]<< std::endl;
-            }
-        }
-};
-
-*/
 
 template <class T, int N>
 class SArray{
@@ -125,6 +20,13 @@ public:
             *this=rhs;
         }
 
+        SArray(const T rhs){
+            *this=rhs;
+        }
+        SArray(std::initializer_list<T> rhs) {
+          std::copy(rhs.begin(), rhs.end(), data);
+        }
+
         template<class U>
         operator SArray<U,N>()  {
             SArray<U,N> arr;
@@ -134,75 +36,75 @@ public:
             return arr;
         }
 
-        SArray& operator= (const T& rhs){
+        virtual SArray& operator= (const T& rhs){
                 for (int i=0;i<N;i++){
                     data[i] = rhs;
                 }
                 return *this;
             }
 
-        SArray& operator= (const T rhs[]){
+        virtual SArray& operator= (const T rhs[]){
                 for (int i=0;i<N;i++){
                     data[i] = rhs[i];
                 }
                 return *this;
             }
 
-        void copyTo (const T lhs[]){
+        virtual void copyTo (T lhs[]) const {
                 for (int i=0;i<N;i++){
                     lhs[i]=data[i];
                 }
         }
 
-        SArray& operator= (const SArray& rhs){
+        virtual SArray& operator= (const SArray& rhs){
                 for (int i=0;i<N;i++){
                     data[i] = rhs[i];
                 }
                 return *this;
             }
-        SArray operator+ (const SArray& rhs) const {
+        virtual SArray operator+ (const SArray& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] + rhs.data[i];
                 }
                 return arr;
             }
-        SArray operator+ (const T& rhs) const {
+        virtual SArray operator+ (const T& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] + rhs;
                 }
                 return arr;
             }
-        SArray operator- (const SArray& rhs) const {
+        virtual SArray operator- (const SArray& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] - rhs.data[i];
                 }
                 return arr;
             }
-        SArray operator- (const T& rhs) const {
+        virtual SArray operator- (const T& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] - rhs;
                 }
                 return arr;
             }
-        SArray operator* (const SArray& rhs) const {
+        virtual SArray operator* (const SArray& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] * rhs.data[i];
                 }
                 return arr;
             }
-        SArray operator* (const T& rhs) const {
+        virtual SArray operator* (const T& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] * rhs;
                 }
                 return arr;
             }
-        SArray operator/ (const SArray& rhs) const {
+        virtual SArray operator/ (const SArray& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] / rhs.data[i];
@@ -210,14 +112,14 @@ public:
                 return arr;
             }
 
-        SArray operator/ (const T& rhs) const {
+        virtual SArray operator/ (const T& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
                     arr.data[i] = this->data[i] / rhs;
                 }
                 return arr;
             }
-        const T sum() const {
+        virtual const T sum() const {
                 T s = T();
                 for (int i=0;i<N;i++){
                     s += data[i];
@@ -232,7 +134,7 @@ public:
             return data[i];
         }
 
-        void print () {
+        virtual void print () {
             for (int i=0;i<N;i++){
                 std::cout<< "data("<<i<<") = "<<data[i]<< std::endl;
             }
@@ -250,6 +152,7 @@ class ArrayND :public SArray<T, lattice::nD>{
 
     public:
 
+    using SArray<T,lattice::nD>::SArray;
     using SArray<T,lattice::nD>::operator=;
 
     template<class U>
--- test_SArray.cpp
+++ test_SArray.cpp
@@ -1,39 +1,49 @@
-#include"container.h"
+#include"sArray.h"
 
 int main() {
 
+    std::cout<<"===========Testing SArray=============="<<std::endl;
     SArray<double,3> a;
     a = 1.10;
 
     a.print();
     std::cout<<a.sum()<<std::endl;
 
-    // Type casting
-//    SArray<int,3>(a).print();
-
 
     (a*2*a).print();
-
-/////////////////////////////////
-    ArrayND<double> c;
+    std::cout<<"===========Testing ArrayND=============="<<std::endl;
+    ArrayND<double> c=4;
+    std::cout<<"print scalar constructor "<<std::endl;
+    c.print();
+    std::cout<<"print scalar assignment "<<std::endl;
     c=2;
     c.print();
 
+    std::cout<<"print + operator "<<std::endl;
     (c+2.1).print();
 
+    std::cout<<"print sum funtion "<<std::endl;
+    c = c+2.1;
+    std::cout<<"sum="<<c.sum()<<std::endl;
 
-    std::cout<<(c+2.5).sum()<<std::endl;
-
-    double arr[2]={1.9,5};
-    ArrayND<double> d;
-    d = arr;
+    std::cout<<"print array constructor "<<std::endl;
+    double arr[2]={1.5,2.0};
+    ArrayND<double> d(arr);
     d.print();
 
-    std::cout<<c.dot(d)<<std::endl;
-    ArrayND<int> f;
-    f=4;
-    std::cout<<d.dot(f)<<std::endl;
+    std::cout<<"print list constructor"<<std::endl;
+    ArrayND<int> g={1,2};
+    g.print();
 
+    std::cout<<"print dot product (int=>double)"<<std::endl;
+    std::cout<<d.dot(g)<<std::endl;
+
+    std::cout<<"print dot product (double=>int)"<<std::endl;
+    std::cout<<g.dot(d)<<std::endl;
+
+    std::cout<<"print int-double-number, type conversion operation"<<std::endl;
+    c = (ArrayND<double>)g*2+d;
+    c.print();
 
     return 0;
 }
