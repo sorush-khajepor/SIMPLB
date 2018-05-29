bfbf9df920502cfbe9477d7a1b510cd3bb9fc79f
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-29 17:05:53 +0100

    intND, boolND added, function names changed, some debug

--- domainDecomposition.h
+++ domainDecomposition.h
@@ -11,43 +11,37 @@ class DomainDecomposition{
 class StructuredDecomposition :public DomainDecomposition {
 
     protected:
-    // Whole domain dimensions
-    int dim[lattice::nD];
-    // Whole domain volume (number of all nodes before considering ghosts)
-    int vol;
+    // Raw whole domain dimensions (before considering ghosts)
+    intND dim;
     // Number of Blocks in the wholde domain  along each (X-Y-Z) direction
-    int nBlock[lattice::nD];
+    intND nBlock;
     // Shows the periodicity of domain along cartesian(X-Y-Z) axes
-    bool periodic[lattice::nD];
+    boolND periodic;
 
     public:
 
-    StructuredDecomposition(const int dim_[], const bool periodic_[]){
+    StructuredDecomposition(const intND dim_, const boolND periodic_){
 
        // Domain initialization
-       vol=1;
-       for (int iD=0;iD<lattice::nD;++iD){
-          dim[iD]=dim_[iD];
-          periodic[iD]=periodic_[iD];
-	      vol*=dim_[iD];
-       }
+          dim=dim_;
+          periodic=periodic_;
 
-       // Set number of blocks in the domain. dimXYZ is an Estimation of Block dimension in X/Y/Z-direction.
-        int dimXYZ = pow((double)getVol()/mpiTools.getSize(),1.0/lattice::nD);
-        for (int iD=0;iD<lattice::nD;++iD){
-            nBlock[iD] = dim[iD]/dimXYZ;
-        }
+       // Set number of blocks in the domain. dimXYZ is an Estimation of Block dimension in X,Y,Z-direction.
+        int dimXYZ = pow((double)dim.computeVol()/mpiTools.getSize(),1.0/lattice::nD);
+        nBlock = dim/dimXYZ;
     }
 
     const int& getNBlock(const int& iD) const{
         return nBlock[iD];
     }
 
+    const intND& getNBlock() const{
+        return nBlock;
+    }
 
-    virtual void computeBlockGeometry(int blockOrigin[],int blockDim[],int blockCartIndex[],int iBlockNeighbor[]){
-        // Note, here number domain nodes/number of cpus should have squared or
-        // cubic root.
-        computeBlockCartIndex(mpiTools.getRank(),blockCartIndex);
+    virtual void computeBlockGeometry(intND blockOrigin,intND blockDim,intND blockCartIndex,intNQ BlockNeighborIndex){
+
+        blockCartIndex=computeBlockCartIndex(mpiTools.getRank());
 
         for (int iD=0;iD<lattice::nD;++iD){
             int remainder = dim[iD]%nBlock[iD];
@@ -59,36 +53,20 @@ class StructuredDecomposition :public DomainDecomposition {
 
         // Setting block neighbors
         for (int iQ=0;iQ<lattice::nQ;++iQ){
-            iBlockNeighbor[iQ]=getNeighbor(iQ);
+            BlockNeighborIndex[iQ]=computeNeighborIndex(iQ);
         }
     
     }
 
-    const int& getVol() const {return vol;}
 
-    int getShortIndex (const int iXYZ[]) {
-        int sum=0;
-        for (int j = 0; j < lattice::nD; j++) {
-            int n = iXYZ[j];
-            for (int k = j+1; k < lattice::nD; k++) {
-                n*= dim[k];
-            }
-            sum += n;
-        }
-        return sum;
+    intND computePeriodicNeighbor (const intND blockCartIndex, const int& iQ ) {
+
+         return (blockCartIndex + lattice::Qvector[iQ]+nBlock)%nBlock;
     }
 
-    void getPeriodicNeighbor (const int iXYZ[], const int& iQ, int iXYZ_neighbor[]) {
-        for (int iD = 0; iD < lattice::nD; iD++) {
-
-            iXYZ_neighbor[iD] = (iXYZ[iD] + lattice::Qvector[iQ][iD]+nBlock[iD])%nBlock[iD];
-        }
-    }
-
-    int getNeighbor (const int& iQ) {
-        int blockCartIndex[lattice::nD];
-        int neighborCartIndex[lattice::nD];
-        computeBlockCartIndex(mpiTools.getRank(),blockCartIndex);
+    int computeNeighborIndex (const int& iQ) {
+        intND neighborCartIndex;
+        intND blockCartIndex=computeBlockCartIndex(mpiTools.getRank());
         bool is_null = false;
         for (int iD = 0; iD < lattice::nD; iD++) {
             if (periodic[iD]){
@@ -104,12 +82,26 @@ class StructuredDecomposition :public DomainDecomposition {
         if (is_null){
             return MPI_PROC_NULL;
         }else{
-            return getShortIndex(neighborCartIndex);
+            return computeBlockSingleIndex(neighborCartIndex);
         }
     }
 
-    void computeBlockCartIndex (const int& i, int iXYZ[]) {
-        int R = i;
+    int computeBlockSingleIndex (const intND BlockCartIndex) {
+            int sum=0;
+            for (int j = 0; j < lattice::nD; j++) {
+                int n = BlockCartIndex[j];
+                for (int k = j+1; k < lattice::nD; k++) {
+                    n*= nBlock[k];
+                }
+                sum += n;
+            }
+            return sum;
+        }
+
+    // compute Cartesian index of a block
+    intND computeBlockCartIndex (const int& blockSingleIndex) {
+        intND iXYZ;
+        int R = blockSingleIndex;
         for (int j = 0; j < lattice::nD; j++) {
             int n =1;
             for (int k = j+1; k < lattice::nD; k++) {
@@ -118,6 +110,7 @@ class StructuredDecomposition :public DomainDecomposition {
             iXYZ[j]= R/n;
             R=R%n;
         }
+        return iXYZ;
     }
 
 };
