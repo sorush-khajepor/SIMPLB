50ec0e0ca57e326edeb4a1287d4f15011712b907
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-30 00:54:19 +0100

    construction because of message moved here

--- block.hh
+++ block.hh
@@ -1,1 +1,21 @@
 #include "block.h"
+
+// Block construction
+Block::Block(const StructuredDecomposition& sd){
+
+    intNQ BlockNeighborIndex;
+
+    // Initialization of geometry
+    sd.computeBlockGeometry(origin,dim,cartIndex,BlockNeighborIndex);
+    vol=dim.computeVol();
+
+    // Memory allocation for nodes
+    nodes = new Node[vol];
+
+    // Set loop limits for actual and ghost  boundaries
+    setBoundaryLimit();
+    setGhostLimit();
+
+    //TODO Set message
+    //message = new Message (boundaryLimit,ghostLimit,BlockNeighborIndex);
+}
