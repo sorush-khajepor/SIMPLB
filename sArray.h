2cb646d6667ed046bf53fab2c7451c82fe0e3740
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-30 12:07:47 +0100

    Base class created

--- sArray.h
+++ sArray.h
@@ -4,12 +4,106 @@
 #include<iostream>
 
 template <class T, int N>
-class SArray{
+class SArrayBase{
 
 protected:
         T data[N];
 
 public:
+        SArrayBase(){
+            *this=0.;
+        }
+        SArrayBase(const T rhs[]){
+            *this=rhs;
+        }
+
+        SArrayBase(const T rhs){
+            *this=rhs;
+        }
+
+        SArrayBase(std::initializer_list<T> rhs) {
+          std::copy(rhs.begin(), rhs.end(), data);
+        }
+
+        template<class U>
+        operator SArrayBase<U,N>()  {
+            SArrayBase<U,N> arr;
+            for (int i=0;i<N;i++){
+                arr[i] = data[i];
+            }
+            return arr;
+        }
+
+        virtual SArrayBase& operator= (const T& rhs){
+                for (int i=0;i<N;i++){
+                    data[i] = rhs;
+                }
+                return *this;
+            }
+
+        virtual SArrayBase& operator= (const T rhs[]){
+                for (int i=0;i<N;i++){
+                    data[i] = rhs[i];
+                }
+                return *this;
+            }
+
+        virtual void copyTo (T lhs[]) const {
+                for (int i=0;i<N;i++){
+                    lhs[i]=data[i];
+                }
+        }
+
+        virtual SArrayBase& operator= (const SArrayBase& rhs){
+                for (int i=0;i<N;i++){
+                    data[i] = rhs[i];
+                }
+                return *this;
+            }
+        virtual SArrayBase operator+ (const SArrayBase& rhs) const {
+                SArrayBase arr;
+                for (int i=0;i<N;i++){
+                    arr.data[i] = this->data[i] + rhs.data[i];
+                }
+                return arr;
+            }
+
+        T& operator[](const int& i){
+            return data[i];
+        }
+        const T& operator[] (const int& i)const{
+            return data[i];
+        }
+
+
+        friend std::ostream& operator<<(std::ostream& os, const SArrayBase& arr){
+            for (int i=0;i<N;i++){
+                os<<std::endl<<"data("<<i<<") = "<<arr[i];
+            }
+            os<<std::endl<<std::endl;
+            return os;
+        }
+};
+
+template <class T, int N>
+class SArray :public SArrayBase<T,N>{
+
+protected:
+    //    T data[N];
+    using SArrayBase<T,N>::data;
+
+public:
+    using SArrayBase<T,N>::SArrayBase;
+    template<class U>
+    operator SArray<U,N>()  {
+        SArray<U,N> arr;
+        for (int i=0;i<N;i++){
+            arr[i] = data[i];
+        }
+        return arr;
+    }
+    
+    /*
         SArray(){
             *this=0.;
         }
@@ -60,6 +154,7 @@ public:
                 }
                 return *this;
             }
+            */
         virtual SArray operator+ (const SArray& rhs) const {
                 SArray arr;
                 for (int i=0;i<N;i++){
@@ -137,7 +232,7 @@ public:
         virtual const T dot(const SArray& arr){
             return (*this * arr).sum();
         }
-
+/*
         T& operator[](const int& i){
             return data[i];
         }
@@ -153,6 +248,7 @@ public:
             os<<std::endl<<std::endl;
             return os;
         }
-};
+        */
 
+};
 #endif
