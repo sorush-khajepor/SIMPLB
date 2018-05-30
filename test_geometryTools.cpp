0d0be84baefa1ed97635c5753372afca229272f5
Stephen Burnside <stephenburnside1@aol.com>
2018-05-30 15:38:45 +0100

    small change

--- test_geometryTools.cpp
+++ test_geometryTools.cpp
@@ -5,18 +5,18 @@ int main (){
     cout<<" Box test:"<<endl;
     Box box({0,1},{6,7}) ;
 
-    std::cout<<box;
+    cout<<box;
 
-    std::cout<<box.computeVol()<<endl;
+    cout<<box.computeVol()<<endl;
 
 
     cout<<" LoopLimit test:"<<endl;
     LoopLimit ll({0,1},{6,7}) ;
 
-    std::cout<<ll;
+    cout<<ll;
 
-    std::cout<<"volume="<<ll.computeVol()<<endl;
-    std::cout<<"dimension="<<ll.computeDim()<<endl;
+    cout<<"volume="<<ll.computeVol()<<endl;
+    cout<<"dimension="<<ll.computeDim()<<endl;
 
     return 0;
 }
