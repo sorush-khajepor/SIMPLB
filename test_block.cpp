dceb4b92e4a58b52dc5412bd531d61e21962bccb
Sorush Khajepor <sorush.kh@gmail.com>
2018-05-31 03:27:43 +0100

    more tests added

--- test_block.cpp
+++ test_block.cpp
@@ -9,9 +9,20 @@ int main(int argc, char const *argv[]) {
     StructuredDecomposition  structuredDecomposition(dim,periodic);
 
     Block block(structuredDecomposition);
+// Uncomment cout one by one
+//    cout<<"~~~~~~~~~~~~~~Block specs for rank = "<<mpiTools.getRank()<<"~~~~~~~~~~~"<<endl<<block;
 
-    cout<<"~~~~~~~~~~~~~~Block specs for rank = "<<mpiTools.getRank()<<"~~~~~~~~~~~"<<endl<<block;
+// if (mpiTools.getRank()==0) std::cout << "boundaryLimit = " << block.getBoundaryLimit()<<'\n';
 
+//if (mpiTools.getRank()==0) std::cout << "ghostLimit = " << block.getGhostLimit()<<'\n';
+
+
+//if (mpiTools.getRank()==0) std::cout << "sendLimit = " << block.getMessage().getSendLimit()<<'\n';
+
+
+//if (mpiTools.getRank()==4) std::cout << "recvLimit = " << block.getMessage().getRecvLimit()<<'\n';
+
+    cout<<"~~~~~~~~~~~~~~Block Neighbors for rank = "<<mpiTools.getRank()<<"~~~~~~~~~~~"<<endl<<block.getMessage().getNeighbor();
 
     MPI_Finalize();
     return 0;
